a1ff70379d59838a3e7b39df59ef73bc53c421cb
amanraj209 <amanraj20996@gmail.com>
2017-07-27 02:44:49 +0530

    Style Nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,27 +1,34 @@
 const React = require('react');
 const {NavLink} = require('react-router-dom');
 
-// let Nav = React.createClass({
-//     render: function () {
-//         return (
-//             <div>
-//                 <NavLink exact to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink>
-//                 <NavLink exact to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
-//                 <NavLink exact to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
-//             </div>
-//         );
-//     }
-// });
+let Nav = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        alert('Not yet wired up!');
+    },
 
-// Stateless functional component (These functions only define render method and they don't maintain any state)
-let Nav = (props) => {
-    return (
-        <div>
-            <NavLink exact to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink>
-            <NavLink exact to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink>
-            <NavLink exact to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink>
-        </div>
-    );
-};
+    render: function () {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li><NavLink exact to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</NavLink></li>
+                        <li><NavLink exact to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</NavLink></li>
+                        <li><NavLink exact to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</NavLink></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li><input type="search" placeholder="Search"/></li>
+                            <li><input type="submit" className="button" value="Get Weather"/></li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
 
 module.exports = Nav;
