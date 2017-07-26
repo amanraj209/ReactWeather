4f65e2c14ff09a5a940bbd590665cb6d5409ff1c
amanraj209 <amanraj20996@gmail.com>
2017-07-27 02:27:50 +0530

    Add foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ const Weather = require('Weather');
 const About = require('About');
 const Examples = require('Examples');
 
+require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
     <HashRouter>
         <div>
